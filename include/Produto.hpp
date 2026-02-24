f1380b177b130611318fc2cdd2d3f884882f4f18
Eduardo Augusto <eduz92337@gmail.com>
2026-02-24 13:16:01 -0300

    Added produto's Template

--- include/Produto.hpp
+++ include/Produto.hpp
@@ -1,17 +1,51 @@
 #pragma once
 
 class Produto {
-private:
+protected:
     std::string nome;
     float valor;
+    int quantidade;
+    int tipo;
+    int idCOntribuinte;
 public:
     Produto() {
-        valor = 0.0f;
+        this->valor = 0.0f;
     }
-
-    void exibir();
-    void atualizar();
+    Produto(std::string nome,float valor,int quantidade,int tipo){
+        this->nome = nome;
+        this->valor = valor;
+        this->quantidade=quantidade;
+        this->tipo=tipo;
+    }
+    virtual void exibir();
+    virtual void atualizar(int qnt);
+    virtual float calcularImposto();
+    virtual std::string getTipo();
 
     std::string getNome() const { return nome; }
     float getValor() const { return valor; }
+    void setValor(int v){this->valor = v;}
+    void setQuantidade(int qnt) {this->quantidade = qnt;}
+    int getQuantidade(){return this->quantidade;}
+
+};
+
+class ProdutoDeLimpeza : public Produto {
+ 
+
+};
+
+class ProdutosEletronicos : public Produto{
+
+};
+
+class ProdutoAlimenticio : public Produto{
+
+
+};
+
+class ProdutoVestuario : public Produto{
+
+
+
 };
